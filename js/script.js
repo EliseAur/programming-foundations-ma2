d8f4cd1b0f0e2606a18d1f161612e5058108795e
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-06-12 12:22:13 +0200

    finished MA 2
    added title

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+//Programming Foundations
+// Module 2
+//Module Assignment 2
+//Elise Aurtande
+
 //Question 1
-//Loop through the array below and console log each type property
-//if the age is at least 4.
+
 var pets = [
     {
         type: "cat",
@@ -25,11 +29,7 @@ for (var i = 0; i < pets.length; i++) {
 }
 
 //Question 2
-//Create a function with one argument.
-//Inside the function check the type of the value being passed in.
 // it is a boolean value, log the value.
-//If it is not a boolean value, log the message “Please pass a boolean value in”.
-//Call the function and pass a value in.
 
 function isLoggedIn(loggedIn) {
     typeOfValue = typeof loggedIn;
@@ -45,10 +45,6 @@ var input = isLoggedIn(true);
 console.log(input);
 
 //Question 3
-//Write code that does the following:
-//When the button in the HTML is clicked,
-//change the colour of the h2 element to “blue” and
-//change it to read “Updated subheading”.
 
 var buttonChange = document.querySelector("button");
 var headingTwo = document.querySelector("h2");
